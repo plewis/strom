64071163ad6efdbd461622e525464222155928b9
Paul O. Lewis <paul.lewis@uconn.edu>
2018-03-31 17:45:34 -0400

    updating part 6

--- src/tree.hpp
+++ src/tree.hpp
@@ -9,17 +9,23 @@ namespace strom
 
     class TreeManip;
     //class Likelihood;
+    //class Updater;
 
     class Tree
         {
 
         friend class TreeManip;
         //friend class Likelihood;
+        //friend class Updater;
 
         public:
 
                                         Tree();
                                         ~Tree();
+
+            bool                        isRooted() const;
+            unsigned                    numLeaves() const;
+            unsigned                    numNodes() const;
 
         private:
 
@@ -53,6 +59,21 @@ namespace strom
         _root = 0;
         _nodes.clear();
         _preorder.clear();
+        }
+
+    inline bool Tree::isRooted() const
+        {
+        return _is_rooted;
+        }
+
+    inline unsigned Tree::numLeaves() const
+        {
+        return _nleaves;
+        }
+
+    inline unsigned Tree::numNodes() const
+        {
+        return (unsigned)_nodes.size();
         }
 
     }
