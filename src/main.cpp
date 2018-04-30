10dd0f0d100778b8fe4a7faef9523afdee21212b
Paul O. Lewis <paul.lewis@uconn.edu>
2018-04-30 19:53:03 -0400

    Part 11

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
-#include "strom.hpp"
+#include "lot.hpp"
 
 using namespace strom;
 
-// static data member initializations
-std::string  Strom::_program_name        = "strom";
-unsigned     Strom::_major_version       = 1;
-unsigned     Strom::_minor_version       = 0;
-const double Node::_smallest_edge_length = 1.0e-12;
-
 int main(int argc, const char * argv[])
     {
-    Strom strom;
-    try {
-        strom.processCommandLineOptions(argc, argv);
-        strom.run();
-    }
-    catch(std::exception & x) {
-        std::cerr << "Exception: " << x.what() << std::endl;
-        std::cerr << "Aborted." << std::endl;
-    }
-    catch(...) {
-        std::cerr << "Exception of unknown type!\n";
-    }
+    Lot lot;
+    lot.setSeed(12345);
+    std::cout << "Seed set to 12345" << std::endl;
+    std::cout << "  Uniform(0,1) random deviate:             " << lot.uniform() << std::endl;
+    std::cout << "  Uniform(0,1) random deviate (log scale): " << lot.logUniform() << std::endl;
+    std::cout << "  Discrete Uniform(1,4) random deviate:    " << lot.randint(1,4) << std::endl;
+    std::cout << "  Normal(0,1) random deviate:              " << lot.normal() << std::endl;
+    std::cout << "  Gamma(2,1) random deviate:               " << lot.gamma(2.0,1.0) << std::endl;
+    lot.setSeed(12345);
+    std::cout << "\nSeed set to 12345" << std::endl;
+    std::cout << "  Uniform(0,1) random deviate:             " << lot.uniform() << std::endl;
+    std::cout << "  Uniform(0,1) random deviate (log scale): " << lot.logUniform() << std::endl;
+    std::cout << "  Discrete Uniform(1,4) random deviate:    " << lot.randint(1,4) << std::endl;
+    std::cout << "  Normal(0,1) random deviate:              " << lot.normal() << std::endl;
+    std::cout << "  Gamma(2,1) random deviate:               " << lot.gamma(2.0,1.0) << std::endl;
 
     return 0;
     }
